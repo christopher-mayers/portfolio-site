7969878a488a8b10efc3e05fde025582e4c537e3
christopher-mayers <chrisandrewmayers@gmail.com>
2022-07-10 14:31:30 -0700

    initial commit

---
+++ src/Stuff.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Stuff extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Stuff</h2>
+                <p>list introduction</p>
+
+                <ol>
+                    <li>item a</li>
+                    <li>item b</li>
+                    <li>item c</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default Stuff;
